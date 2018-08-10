7fa37d8af6c5af6fd650669633815bdcb8462d0c
Deboprity.Das <pritydas001@gmail.com>
2018-08-10 11:36:45 -0500

    populating values in view and edit page

--- app/src/main/java/com/example/android/storeinventory/EditorActivity.java
+++ app/src/main/java/com/example/android/storeinventory/EditorActivity.java
@@ -3,6 +3,7 @@ package com.example.android.storeinventory;
 import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
@@ -337,12 +338,49 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
+        Log.d(TAG, "onCreateLoader: started");
+        // Since the editor shows all item attributes, define a projection that contains
+        // all columns from the pet table
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_ITEM_NAME,
+                InventoryEntry.COLUMN_ITEM_DESC,
+                InventoryEntry.COLUMN_ITEM_QUANTITY,
+                InventoryEntry.COLUMN_ITEM_PRICE };
+        Log.d(TAG, "onCreateLoader: projection :: "+projection.length);
+        Log.d(TAG, "onCreateLoader: mCurrentItemUri :: "+mCurrentItemUri);
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                mCurrentItemUri,         // Query the content URI for the current item
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        Log.d(TAG, "onLoadFinished: started");
+        if (cursor.moveToFirst()) {
+            // Find the columns of item attributes that we're interested in
+            int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
+            int descColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_DESC);
+            int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
+            int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
 
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColumnIndex);
+            String description = cursor.getString(descColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            mItemName.setText(name);
+            mItemDesc.setText(description);
+            mItemQuantity.setText(Integer.toString(quantity));
+            mItemPrice.setText(Integer.toString(price));
+        }
+        Log.d(TAG, "onLoadFinished: ended");
     }
 
     @Override
